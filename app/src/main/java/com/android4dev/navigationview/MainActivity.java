dd0e026299bc60fcc449b7774dca3d122ad06317
victory1908 <victory1908@gmail.com>
2015-12-11 15:33:18 +0800

    dependance

--- app/src/main/java/com/android4dev/navigationview/MainActivity.java
+++ app/src/main/java/com/android4dev/navigationview/MainActivity.java
@@ -118,11 +118,6 @@ public class MainActivity extends AppCompatActivity {
         //calling sync state is necessay or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
 
-
-
-
-
-
     }
 
     @Override
